c09791a2251953a1ef84b92b08a33129850d27e8
luchev <luchevz@gmail.com>
2020-06-10 19:26:20 +0300

    Add script to add systemd config for the daemon

---
+++ setup/add-daemon-to-systemd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+projectRoot="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
+
+cat > "/etc/systemd/system/barry.service" << EOF
+[Unit]
+Description=Barry speech recognition
+
+[Service]
+Type=forking
+User=root
+ExecStart=${projectRoot}/daemon/barryd.py start
+
+[Install]
+WantedBy=multi-user.target
+EOF
