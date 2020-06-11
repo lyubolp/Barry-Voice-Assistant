9927e5663a73c0675c50e18551b87a29d422dc95
Lyuboslav Karev <lyubo.karev@gmail.com>
2020-06-11 14:13:58 +0300

    Changed req.sh

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,3 @@
 # apt-installs
 
-apt-get install gcc make pkg-config automake libtool libasound2-dev git curl python3 pip3 -y;
+apt-get install gcc make pkg-config automake libtool libasound2-dev git curl python3 python3-pip -y;
