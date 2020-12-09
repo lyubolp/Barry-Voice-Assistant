be5b4b9209f36f7d4a68b277eb1ec005b389c7f3
Lyubo <lyubo.karev@gmail.com>
2020-12-09 23:21:37 +0200

    Added the alarm page

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+
+from alarm import SendAlarmData
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
 @app.route('/')
@@ -9,8 +13,25 @@ def index():
     user = {
         'username': 'lyubolp',
         'name': 'Lyubo'
-            }
+    }
     return render_template('index.html', title='Home', user=user)
 
+
+@app.route('/alarm')
+def alarm():
+    alarm = {
+        'ring': False,
+        'time': '08:30',
+    }
+    form = SendAlarmData()
+    return render_template('alarm.html', title='Alarm', alarm=alarm, form=form)
+
+@app.route('/submit-alarm', methods=['GET', 'POST'])
+def login():
+    form = SendAlarmData()
+    if form.validate_on_submit():
+        print(form.alarm_time.data)
+        return redirect('/')
+    return render_template('alarm.html', title='Alarm', alarm=alarm, form=form)
 if __name__ == '__main__':
     app.run()
