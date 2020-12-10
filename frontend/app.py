46bad536a60e13b1f2a0f7f67547a0547fb6e628
Lyubo <lyubo.karev@gmail.com>
2020-12-10 10:38:20 +0200

    Added some connections to the client API

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,11 @@
+import json
+
 from flask import Flask, render_template, flash, redirect
 
 from alarm import SendAlarmData
 from config import Config
+from client import client
+from types import SimpleNamespace
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -26,12 +30,44 @@ def alarm():
     form = SendAlarmData()
     return render_template('alarm.html', title='Alarm', alarm=alarm, form=form)
 
+
 @app.route('/submit-alarm', methods=['GET', 'POST'])
-def login():
+def submit_alarm():
     form = SendAlarmData()
     if form.validate_on_submit():
         print(form.alarm_time.data)
         return redirect('/')
     return render_template('alarm.html', title='Alarm', alarm=alarm, form=form)
+
+
+@app.route('/news')
+def news():
+    try:
+        token = client.login('luchevz@gmail.com', '123123')
+        # Execute command
+        try:
+            response = client.execute_command(token, "news about sport")
+
+            news_objects = []
+
+            print("Response: ", response)
+
+            for news_piece in response['details']:
+                print(news_piece)
+                news_objects.append(json.loads(news_piece))
+            # x = json.loads(response, object_hook=lambda d: SimpleNamespace(**d))
+            # print(x.name, x.hometown.name, x.hometown.id)
+        except Exception as err:
+            print("Failed to execute command :(")
+            print(err)
+
+        print()
+    except Exception as err:
+        print("Failed to login :(")
+        print(err)
+
+    return render_template('news.html', title='News')
+
+
 if __name__ == '__main__':
     app.run()
