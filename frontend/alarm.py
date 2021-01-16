1a4eb81a0755dc01f97498fa895f018650e8c29d
Lyubo <lyubo.karev@gmail.com>
2021-01-16 07:03:11 +0200

    Removed unneeded files

--- frontend/alarm.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms.fields.html5 import TimeField
-from wtforms.validators import DataRequired
-
-
-class SendAlarmData(FlaskForm):
-    alarm_time = TimeField('Alarm time')
