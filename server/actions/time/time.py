5f082962e8d2b6ea74c260e482ed6f8c787e5b89
luchev <luchevz@gmail.com>
2021-01-17 12:28:45 +0200

    Feat time action now returns details

--- server/actions/time/time.py
+++ server/actions/time/time.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import datetime
 import inflect
+import json
 
 
 def execute_action():
@@ -9,8 +10,10 @@ def execute_action():
     minutes = time.minute
 
     p = inflect.engine()
-    return "The time is " + p.number_to_words(hour) + " " + p.number_to_words(minutes)
-
+    time_words = p.number_to_words(hour) + " " + p.number_to_words(minutes)
+    message = "The time is " + time_words
+    time_numeric = str(hour) + ':' + str(minutes)
+    return json.dumps({'message': ''.join(message), 'details': {'time_numeric': time_numeric, 'time_words': time_words}})
 
 if __name__ == '__main__':
     print(execute_action())
